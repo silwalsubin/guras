619176642fc4b5cd440e12179c2ca1bf17f576ef
Subin Silwal <silwalsubin@gmail.com>
2025-07-06 21:25:22 -0500

    fix(terraform): add timeout to certificate validation and remove manual DNS steps from workflow

--- iac/terraform/modules/ssl/main.tf
+++ iac/terraform/modules/ssl/main.tf
@@ -30,8 +30,12 @@ resource "aws_route53_record" "cert_validation" {
   zone_id         = var.route53_zone_id
 }
 
-# Certificate validation
+# Certificate validation with timeout
 resource "aws_acm_certificate_validation" "main" {
   certificate_arn         = aws_acm_certificate.main.arn
   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+  
+  timeouts {
+    create = "10m"
+  }
 } 
