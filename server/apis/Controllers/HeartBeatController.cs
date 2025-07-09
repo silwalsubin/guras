0c86613e8e6a049155b85dce6c0d4d8a3efabf00
Subin Silwal <silwalsubin@gmail.com>
2025-07-08 23:55:22 -0500

    Update HeartBeatController with additional changes

--- server/apis/Controllers/HeartBeatController.cs
+++ server/apis/Controllers/HeartBeatController.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Security.Claims;
 using apis.Configuration;
 using Dapper;
 using Npgsql;
@@ -28,25 +27,5 @@ public class HeartBeatController(ILogger<HeartBeatController> logger, DbConnecti
         await using var connection = new NpgsqlConnection(connectionString);
         var sqlServerTime = await connection.QuerySingleAsync<DateTime>("SELECT NOW()");
         return Ok(sqlServerTime);
-    }
-
-    [Authorize]
-    [HttpGet("authenticated")]
-    public ActionResult<object> GetAuthenticated()
-    {
-        var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-        var email = User.FindFirst(ClaimTypes.Email)?.Value;
-        var name = User.FindFirst(ClaimTypes.Name)?.Value;
-
-        logger.LogInformation("Authenticated HelloWorld endpoint called by user {UserId}", userId);
-
-        return Ok(new
-        {
-            message = "Hello from authenticated endpoint!",
-            userId,
-            email,
-            name,
-            timestamp = DateTime.UtcNow
-        });
     }
 } 
