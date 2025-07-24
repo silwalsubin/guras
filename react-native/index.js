4123146af74351fd7ff75f87c018d153042040b4
Subin Silwal <silwalsubin@gmail.com>
2025-07-23 23:00:31 -0500

    Enable background audio playback with TrackPlayer service

--- react-native/index.js
+++ react-native/index.js
@@ -2,8 +2,12 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import {AppRegistry} from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
+import {name as appName} from './app.json';
+import TrackPlayer from 'react-native-track-player';
+import trackPlayerService from './trackPlayerService';
+
+TrackPlayer.registerPlaybackService(() => trackPlayerService);
 
 AppRegistry.registerComponent(appName, () => App);
