48c2120b8ad9a86530ccd61f942905f2e2e1cf4b
Subin Silwal <silwalsubin@gmail.com>
2025-07-24 09:27:24 -0500

    fix: use Event enum for remote play/pause in trackPlayerService.js for better compatibility

--- react-native/trackPlayerService.js
+++ react-native/trackPlayerService.js
@@ -1,7 +1,7 @@
-import TrackPlayer from 'react-native-track-player';
+import TrackPlayer, { Event } from 'react-native-track-player';
 
 module.exports = async function() {
-  TrackPlayer.addEventListener('remote-play', () => TrackPlayer.play());
-  TrackPlayer.addEventListener('remote-pause', () => TrackPlayer.pause());
+  TrackPlayer.addEventListener(Event.RemotePlay, () => TrackPlayer.play());
+  TrackPlayer.addEventListener(Event.RemotePause, () => TrackPlayer.pause());
   // Add more event listeners as needed
 }; 
