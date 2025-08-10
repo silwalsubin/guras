d3b51a3fe64da832ffab09b92fac29d2223e72d4
Subin Silwal <silwalsubin@gmail.com>
2025-08-10 00:25:34 -0500

    push changes

--- server/services.notifications/Services/IUserNotificationPreferencesService.cs
+++ server/services.notifications/Services/IUserNotificationPreferencesService.cs
@@ -7,6 +7,8 @@ public interface IUserNotificationPreferencesService
     Task<UserNotificationPreferences?> GetUserPreferencesAsync(string userId);
     Task<UserNotificationPreferences> CreateOrUpdateUserPreferencesAsync(UserNotificationPreferences preferences);
     Task<List<UserNotificationPreferences>> GetUsersDueForNotificationAsync();
+    Task<List<UserNotificationPreferences>> GetAllUserPreferencesAsync();
+    Task<bool> IsUserDueForNotificationAsync(string userId);
     Task UpdateLastNotificationSentAsync(string userId);
     Task<bool> IsUserInQuietHoursAsync(string userId);
 }
